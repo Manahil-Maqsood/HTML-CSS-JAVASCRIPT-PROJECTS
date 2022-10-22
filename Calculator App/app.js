afd623b6d5caefb8c5795353fb2b2be3cb79fc32
Manahil Maqsood <manahilmaqsood142@gmail.com>
2022-10-22 18:31:21 +0500

    Complete Calculator App

---
+++ Calculator App/app.js
@@ -0,0 +1,27 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let buttonClear = document.querySelector(".btn-clear");
+  let buttonEqual = document.querySelector(".btn-equal");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+  buttonEqual.addEventListener("click", function (e) {
+    if(screen.value === ""){
+        screen.value = "";
+    }else{
+        let answer = eval(screen.value);
+        screen.value = answer;
+    }
+  });
+
+  buttonClear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+
+})();
